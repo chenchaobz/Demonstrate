ed32f1e46114756faa496623195114dcd43ef832
cc <cehnchao_bz@163.com>
2019-07-19 17:01:07 +0800

    数字倒转

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Algorithm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,18 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
+
+
+            ReverseInteger reverseInteger = new ReverseInteger();
+            int reverseInteger_test1 = reverseInteger.Reverse_A(-2147483412);
+            Console.WriteLine("输入：123，输出："+reverseInteger_test1);
+            //int reverseInteger_test2 = reverseInteger.Reverse_A(-1);
+            //Console.WriteLine("输入：-123，输出：" + reverseInteger_test2);
+            //int reverseInteger_test3 = reverseInteger.Reverse_A(4);
+            //Console.WriteLine("输入：120，输出：" + reverseInteger_test3);
+            //Console.WriteLine(length);
+            Console.ReadLine();
+
         }
     }
 }
